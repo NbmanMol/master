0cc2a48b6a76270452b3aec9e07b42bc07133bfc
NbmanMol <125892325+NbmanMol@users.noreply.github.com>
2023-12-29 19:47:45 +0500

    Delete src/main directory

--- src/main/java/ru/kata/spring/boot_security/demo/service/RoleService.java
+++
@@ -1,8 +0,0 @@
-package ru.kata.spring.boot_security.demo.service;
-
-import ru.kata.spring.boot_security.demo.model.Role;
-
-public interface RoleService {
-    void save(Role role);
-    Role findByRolename(String rolename);
-}
